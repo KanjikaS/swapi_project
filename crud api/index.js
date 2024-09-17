dc792308ca53e9d6d8755f2974c5fd257027c6fe
Kanjika <kanjika@Kanjikas-MacBook-Pro.local>
2024-09-17 11:37:30 +0530

    crud api

---
+++ crud api/index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+let users = [];
+
+
+const seedUsers = () => {
+    users = [];
+    for (let i = 1; i <= 10; i++) {
+        users.push({
+            id: i,
+            name: `User${i}`,
+            email: `user${i}@example.com`,
+        });
+    }
+};
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+});
+
+app.post('/users', (req, res) => {
+    const { name, email} = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({ message: "Name and email are required" });
+    }
+    const emailExists = users.some(user => user.email === email);
+    if (emailExists) {
+        return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const newUser = {
+        id: users.length ? users[users.length - 1].id + 1 : 1,
+        name,
+        email,
+    };
+
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const { name, email } = req.body;
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+
+    res.json(user);
+});
+app.patch('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const { name, email } = req.body;
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    
+    if (email && users.some(u => u.email === email && u.id !== userId)) {
+        return res.status(400).json({ message: "Email already exists" });
+    }
+
+    
+    if (name) user.name = name;
+    if (email) user.email = email;
+
+    res.json(user);
+});
+
+
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === userId);
+
+    if (userIndex === -1) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    users.splice(userIndex, 1); 
+    res.status(204).send();      
+});
+
+app.listen(PORT, () => {
+    seedUsers(); 
+    console.log(`Server is running on port ${PORT}`);
+});
